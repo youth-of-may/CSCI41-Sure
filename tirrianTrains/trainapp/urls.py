b50b14fd0bae556e65db40163f38300080264123
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-11-22 23:43:13 +0800

    finished basic logic for loading the booking view

--- tirrianTrains/trainapp/urls.py
+++ tirrianTrains/trainapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from trainapp.views.customer import create_customer
+from trainapp.views.booking import booking_page
 
 urlpatterns = [
     path('register/', create_customer, name='customer_creation'),
+    path('booking/', booking_page, name='booking_page'),
 ]
--- tirrianTrains/trainapp/views/booking.py
+++ tirrianTrains/trainapp/views/booking.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from trainapp.services.booking_sql import list_local_trips, list_intertown_trips
 
+def booking_page(request):
+    ''' Shows all available bookings, separated into local and inter-town '''
+    local_trips = list_local_trips()
+    intertown_trips = list_intertown_trips()
+    
+    # Currently does not actually load the trips since the template is showing hardcoded stuff
+    return render(request, "trainapp/booking.html", {
+        "local_trips": local_trips,
+        "intertown_trips": intertown_trips,
+    })
