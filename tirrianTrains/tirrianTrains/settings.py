2bf9dc698a4fcfb6a0ead30dfdfe79ec60728768
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2025-11-22 15:40:03 +0800

    Changed the name of the database in settings.

--- tirrianTrains/tirrianTrains/settings.py
+++ tirrianTrains/tirrianTrains/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'tirrianTrains.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # for mysqlclient
-        'NAME': 'tirrianTrain',
+        'NAME': 'tirriantrainsdb',
         'USER': 'root',          
         'PASSWORD': '',
         'HOST': 'localhost',     
