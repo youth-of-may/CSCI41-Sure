afcfe7acdd4c5013ec07c0b56b54dc0d496a6c22
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2025-11-23 16:33:26 +0800

    Fixed create customer view.

--- tirrianTrains/trainapp/urls.py
+++ tirrianTrains/trainapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from trainapp.views.customer import create_customer
+from trainapp.views.customer import create_customer_view
 from trainapp.views.booking import booking_page
 
 urlpatterns = [
-    path('register/', create_customer, name='customer_creation'),
+    path('register/', create_customer_view, name='customer_creation'),
     path('booking/', booking_page, name='booking_page'),
 ]
--- tirrianTrains/trainapp/views/customer.py
+++ tirrianTrains/trainapp/views/customer.py
@@ -4,14 +4,8 @@ from trainapp.services.customer_sql import create_customer
 """
 imports the logic from trainapp and uses it to create a new customer
 """
-def create_customer(request):  
+def create_customer_view(request):  
     if request.method=='POST':
         if request.method == 'POST':
-            cid = create_customer(
-            request.POST['lastName'],
-            request.POST['givenName'],
-            request.POST.get('middleInitial'),
-            request.POST['birthDate'],
-            request.POST.get('gender')
-            )
+            cid = create_customer(request.POST['lastName'], request.POST['givenName'], request.POST.get('middleInitial'),request.POST['birthDate'], request.POST.get('gender'))
     return render(request, "trainapp/customer_form.html")
