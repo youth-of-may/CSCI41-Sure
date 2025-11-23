f6442505404a4eaf6287f62fe71d9d55aca19f8a
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2025-11-23 17:44:18 +0800

    Created a trips view.

--- tirrianTrains/trainapp/urls.py
+++ tirrianTrains/trainapp/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from trainapp.views.customer import create_customer_view
 from trainapp.views.booking import booking_page
 from trainapp.views.maintenance import *
+from trainapp.views.trips import * 
 
 urlpatterns = [
     path('register/', create_customer_view, name='customer_creation'),
     path('booking/', booking_page, name='booking_page'),
     path('train/', get_trains, name="train_list"),
     path('train/<int:pk>/details', get_maintenance_history, name="train_detail"),
+    path('trips/', schedules_list, name="schedule_list"),
+    path('trips/<str:trip_date>/details', schedule_detail, name="schedule_detail"),
 ]
