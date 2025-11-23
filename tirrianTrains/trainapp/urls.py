896b9f2ed01d0e5b3c9083ad6a89364c8db3313b
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2025-11-23 18:32:53 +0800

    Added new urls for stations/routes

--- tirrianTrains/trainapp/urls.py
+++ tirrianTrains/trainapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from trainapp.views.customer import create_customer_view
 from trainapp.views.booking import booking_page
 from trainapp.views.maintenance import *
-from trainapp.views.trips import * 
+from trainapp.views.trips import *
+from trainapp.views.route import * 
 
 urlpatterns = [
     path('register/', create_customer_view, name='customer_creation'),
@@ -11,4 +12,6 @@ urlpatterns = [
     path('train/<int:pk>/details', get_maintenance_history, name="train_detail"),
     path('trips/', schedules_list, name="schedule_list"),
     path('trips/<str:trip_date>/details', schedule_detail, name="schedule_detail"),
+    path('stations/', stations_list, name="station_list"),
+    path('stations/<int:destinationID>/details', station_detail, name="station_detail"),
 ]
