f252c17e954fa4d7e4a09944474da31fd70493e9
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-11-24 23:39:25 +0800

    added basic logic to view price_history of specific routes

--- tirrianTrains/trainapp/views/route.py
+++ tirrianTrains/trainapp/views/route.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from trainapp.services.station_sql import *
 from trainapp.services.route_sql import *
+from trainapp.services.price_history_sql import *
+from django.http import HttpResponse
 
 
 def stations_list(request):
@@ -16,3 +18,16 @@ def station_detail(request, destinationID):
         'routes': get_destination_routes(destinationID),
     }
     return render(request, "trainapp/stations/station_detail.html", context)
+
+def station_price_history(request, routeID):
+    ''' Show the price changes made on a specific route '''
+    if request.method == 'POST':
+        # Will insert logic to allow price change later on
+        return(HttpResponse("<h1>Under Progress hehe</h1>"))
+    else:
+        return render(request, "trainapp/price_history.html",{'routePriceHistory':generate_price_history(routeID)})
+
+# Unsure if I'll use this or not pa
+def total_price_history(request):
+    ''' Generate a full list of all changes to price across all stations '''
+    return render(request, "trainapp/stations/station_detail.html", {'routePriceHistory':full_price_history()})
