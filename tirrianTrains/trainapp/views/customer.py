e1ca2c0b5132781915bda7d5a1bd393c276b56df
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2025-11-29 13:36:38 +0800

    Update customer.py

--- tirrianTrains/trainapp/views/customer.py
+++ tirrianTrains/trainapp/views/customer.py
@@ -7,7 +7,6 @@ imports the logic from trainapp and uses it to create a new customer
 """
 def create_customer_view(request):  
     if request.method=='POST':
-        if request.method == 'POST':
             cid = create_customer(request.POST['lastName'], request.POST['givenName'], request.POST.get('middleInitial'),request.POST['birthDate'], request.POST.get('gender'), request.POST.get('email'))
             messages.success(request, "User created successfully!")
             return redirect("homepage")
